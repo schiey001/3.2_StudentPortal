2db514afd8b2114fd309ec29eada9b26f31686d4
yorick <y.vanschie@aashq.nl>
2020-03-09 17:18:48 +0100

    2nd

--- app/src/main/java/com/example/studentportal/MainActivity.kt
+++ app/src/main/java/com/example/studentportal/MainActivity.kt
@@ -72,11 +72,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 ADD_PORTAL_REQUEST_CODE -> {
-                    val reminder = data!!.getParcelableExtra<Portal>(EXTRA_PORTAL)
-                    portals.add(reminder)
+                    val portal = data!!.getParcelableExtra<Portal>(EXTRA_PORTAL)
+                    portals.add(portal)
                     portalAdapter.notifyDataSetChanged()
                 }
             }
